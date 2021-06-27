693a858a8dd8c93f769c189101f66aa1d2c3d824
cypher4859 <obsidia_framework_notification@opaquecipher.33mail.com>
2021-06-27 12:24:32 -0400

    Just got basic RPC working and started fleshing out the docker functionality

--- ContainerManager/docker_manager.py
+++ ContainerManager/docker_manager.py
@@ -1,25 +1,49 @@
 from concurrent import futures
-import random
 import os
-from flask import Flask, render_template
 import grpc
-
+import docker_manager_pb2_grpc
+import docker
 from docker_manager_pb2 import (
     ContainerAction,
     ContainerManagerRequest,
     ContainerManagerResponse
 )
-from docker_manager_pb2_grpc import ContainerManagerStub
 
-app = Flask(__name__)
+client = docker.from_env()
 
-docker_manager_host = os.getenv("DOCKER_MANAGER_HOST", "localhost")
-docker_manager_channel = grpc.insecure_channel(
-    f"{docker_manager_host}:50051"
-)
-docker_manager_client = ContainerManagerStub
+class ContainerManagerService(docker_manager_pb2_grpc.ContainerManagerServicer):
+    def ManageContainer(self, request: ContainerManagerRequest, context) -> ContainerManagerResponse:
+        # Do the docker things here
+        container = client.containers.get(request.containerId)
+        if request.action == ContainerAction.UP:
+            print('bring it up')
+            # Check that the container exists and is currnetly down
+        elif request.action == ContainerAction.DOWN:
+            print('bring it down')
+            container.kill()
+        elif request.action == ContainerAction.RECREATE:
+            print('rebuild')
+            imgae = ''
+            container.kill()
+            container.remove()
+            client.images.pull()
+            newContainer = client.containers.create()
+            newContainer.start()
+            
+        elif request.action == ContainerAction.STATUS:
+            print('return status')
+        else:
+            print('dont know what happened')
+        containerResult = client.containers.list()
+        print(containerResult)
+        return ContainerManagerResponse(containerId=request.containerId, result=f"result: {containerResult}")
 
-@app.route("/docker-manage/up")
-def dockerContainerUp():
-    # startup the container
-    print('Its working')
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    docker_manager_pb2_grpc.add_ContainerManagerServicer_to_server(ContainerManagerService(), server)
+    server.add_insecure_port("[::]:50051")
+    server.start()
+    server.wait_for_termination()
+
+if __name__ == "__main__":
+    serve()
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Black-Stone-API
 A simple flask API used to control various microservices written in Python using gRPC and ProtoBuffs
+
+- Create the protobuf type file
+- Generate the python code making sure to change the directories with:
+`python -m grpc_tools.protoc -I ../protobufs --python_out=. --grpc_python_out=. ../protobufs/recommendations.proto`
